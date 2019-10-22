932c0c4e9ada38a616922fe445ad3de864479d86
bogdanov <bogdanov@comepay.ru>
2019-10-22 17:13:13 +0300

    add result page rtk

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const telegram = require('telegram-bot-api');
 const axios = require('axios');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 const api = new telegram({
   token: config.token,
   updates: {enabled: false}
@@ -20,32 +22,25 @@ const urlConfig = {
   console.log('res', res);
 });*/
 
-app.use('/', (req, res) => {
-  axios.request(urlConfig)
-    .then((res) => {
-      console.log('res', res);
-      console.log(res.status);
-      if (res.status === 200) {
-        console.log('===========================================================================');
-        console.log('==== Proxy server connected');
-        console.log('===========================================================================');
-      }
-    })
-    .catch((err) => {
-      if (!err.response) return console.log('Use next proxy');
-    });
+app.use('/', (req, res, next) => {
+  const body = axios.request(urlConfig)
+    .then(result => result.data)
+    .catch(err => !err.response && console.log('Use next proxy'));
 
-  res.send('Hello TELEGRAM');
+  body.then(data => {
+    res.send(data || 'Hello Telegram');
+    next();
+  });
 });
 
 app.listen(config.port, () => {
   console.log('===========================================================================');
   console.log('= For testing you need to create dedicated bot through BotFather');
   console.log(`=    Bot name: ${config.botName}`);
-  console.log(`=    telegramApiKey: ${config.token}`);
-  console.log(`=    telegramChatId: ${config.chatId}`);
-  console.log(`=    telegramForwardMessageId: ${config.message_id}`);
-  console.log(`=    server listener on port: ${config.port}`);
+  console.log(`=    TelegramApiKey: ${config.token}`);
+  console.log(`=    TelegramChatId: ${config.chatId}`);
+  console.log(`=    TelegramForwardMessageId: ${config.message_id}`);
+  console.log(`=    Server listener on port: ${config.port}`);
   console.log('= ');
   console.log('= These environment variables are only needed for running "node server.js"');
   console.log('===========================================================================');
