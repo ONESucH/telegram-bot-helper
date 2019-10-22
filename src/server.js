a0883687bf11afbe31db6d7bce0b2798300622f5
bogdanov <bogdanov@comepay.ru>
2019-10-22 16:32:25 +0300

    use proxy server

--- src/server.js
+++ src/server.js
@@ -1,36 +1,51 @@
 const config = require('./config');
 const express = require('express');
 const telegram = require('telegram-bot-api');
+const axios = require('axios');
 
 const app = express();
 const api = new telegram({
   token: config.token,
-  updates: {enabled: true}
+  updates: {enabled: false}
 });
-const url = `https://api.telegram.org/bot${config.token}/getMe`;
-console.log(api); // <<-- Лежат методы отправок изображений, сообщений, редактирования и т.п.
-console.log(url);
+const urlConfig = {
+  url: `https://api.telegram.org/bot${config.token}/getMe`,
+  proxy: {
+    host: '104.143.244.64',
+    port: 443
+  }
+};
 
-api.getMe().then(data => {
-  console.log('----->>data', data);
-})
-  .catch((err) => {
-    console.log('----->>err', err);
-  });
+/*api.getMe().then(res => {
+  console.log('res', res);
+});*/
 
+app.use('/', (req, res) => {
+  axios.request(urlConfig)
+    .then((res) => {
+      console.log('res', res);
+      console.log(res.status);
+      if (res.status === 200) {
+        console.log('===========================================================================');
+        console.log('==== Proxy server connected');
+        console.log('===========================================================================');
+      }
+    })
+    .catch((err) => {
+      if (!err.response) return console.log('Use next proxy');
+    });
 
-api.on('message', (message) => {
-  console.log('----->>message', message);
+  res.send('Hello TELEGRAM');
 });
 
 app.listen(config.port, () => {
   console.log('===========================================================================');
   console.log('= For testing you need to create dedicated bot through BotFather');
-  console.log(`= Bot name: ${config.botName}`);
-  console.log('= Export following environment variables:');
+  console.log(`=    Bot name: ${config.botName}`);
   console.log(`=    telegramApiKey: ${config.token}`);
   console.log(`=    telegramChatId: ${config.chatId}`);
   console.log(`=    telegramForwardMessageId: ${config.message_id}`);
+  console.log(`=    server listener on port: ${config.port}`);
   console.log('= ');
   console.log('= These environment variables are only needed for running "node server.js"');
   console.log('===========================================================================');
